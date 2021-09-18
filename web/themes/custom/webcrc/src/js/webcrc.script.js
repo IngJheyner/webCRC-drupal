9ccb5dad406e282c8dc77e6ba826178048a733c4
programador <programador3d@3dstudioweb.com>
2021-09-17 19:14:00 -0500

    compilare script

--- web/themes/custom/webcrc/src/js/webcrc.script.js
+++ web/themes/custom/webcrc/src/js/webcrc.script.js
@@ -8,20 +8,19 @@ import anime from 'animejs/lib/anime.es';
   'use strict';
 
   Drupal.behaviors.helloWorld = {
-    attach: function (context, settings) 
-    {
-      $(document, context).once('webcrc').each( function() 
-      {
-        $(document).ready(function()
-        {
+    attach: function (context, settings) {
+
+      $(document, context).once('webcrc').each( function () {
+
+        $(document).ready( function () {
+
           var indicatorCount = document.querySelectorAll('.count_indicator');
 
-          if(indicatorCount)
-          {
-            indicatorCount.forEach(count_indicator => 
-            {
-              const valueCount = parseFloat(count_indicator.getAttribute('data-target'));
+          if(indicatorCount) {
 
+            indicatorCount.forEach(count_indicator => {
+
+              const valueCount = parseFloat(count_indicator.getAttribute('data-target'));
               anime({
                 targets: count_indicator,
                 innerHTML: [0, valueCount],
@@ -30,7 +29,7 @@ import anime from 'animejs/lib/anime.es';
               });
             });
           }
-        }); 
+        });
       });
     }
   };
