75cb8e35fc45429afe286d8b5e5679ebd516af89
programador <programador3d@3dstudioweb.com>
2021-10-11 12:22:03 -0500

    Pagina de noticias y de niños

--- web/themes/custom/webcrc/src/js/webcrc.script.js
+++ web/themes/custom/webcrc/src/js/webcrc.script.js
@@ -12,8 +12,7 @@ import anime from 'animejs/lib/anime.es';
 
       $(document, context).once('webcrc').each( function () {
 
-        $(document).ready( function () {
-
+        $(document).ready( function () {          
           var indicatorCount = document.querySelectorAll('.count_indicator');
 
           if(indicatorCount) {
