5aa5ea04d0c275f9f1a54b1c9fa3435f16162b76
wodby <wodby@example.com>
2021-10-16 03:49:26 +0000

    Vista pagina biblioteca virtual tabla

--- web/modules/custom/webcrc_projects_regulatory/js/webcrc-projects-regulatory.js
+++ web/modules/custom/webcrc_projects_regulatory/js/webcrc-projects-regulatory.js
@@ -131,46 +131,6 @@
                 ftnInter(code, iframeC, nameRouteFile);
                         
             });
-            
-            /*===========================================
-            TABLE JQUERY VIEW PROJECTS COMMENTS
-            =============================================*/
-            //$('[data-toggle="tooltip"]').tooltip();
-             
-            function filterColumn ( i ) {
-                $('#table__projects-regulatory').DataTable().column( i ).search(
-                    $('#col'+i+'_filter').val(),
-                    //$('#col'+i+'_regex').prop('checked'),
-                    //$('#col'+i+'_smart').prop('checked')
-                ).draw();
-            }
-             
-            $('#table__projects-regulatory').once('webcrc').each(function () {
-                $('#table__projects-regulatory').dataTable({
-                    "searching": false,
-                    "order": [],
-                    "info": false,
-                    "paging": false,
-                    "lengthChange": false,
-                    "responsive": true,
-                    "columnDefs": [
-                        { className: "max-desktop", responsivePriority: 1, targets: 0 },
-                        { className: "max-desktop", responsivePriority: 2, targets: 1 },
-                        { className: "tablet-l desktop", targets: 2 },
-                        { className: "tablet-l desktop", targets: 3 },
-                        { className: "tablet-l desktop", targets: 4 },
-                        { className: "tablet-l desktop", targets: 5 },
-                        { className: "tablet-l desktop", targets: 6 }
-                    ],
-                });
-
-                $('input.column_filter').on( 'keyup click', function () {
-                    filterColumn( $(this).parents('div').attr('data-column') );
-                });
-                $('select.column_filter').on( 'change', function () {
-                    filterColumn( $(this).parents('div').attr('data-column') );
-                });
-            }); 
 
         }
     };
